a203bb16c23d0ec51dc3f564326e8b72a965da78
NaniSkinner <skinner.galvez@gmail.com>
2025-05-20 13:09:56 -0500

    Public Key Cryptography - Hash Message

--- index.js
+++ index.js
@@ -20,3 +20,24 @@ function findColor(hash) {
 module.exports = findColor;
 
 // Find the color that created the hash
+
+// Public Key Cryptography - Hash Message
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+  // Step 1: Convert the string message to an array of UTF-8 bytes
+  const messageBytes = utf8ToBytes(message);
+
+  // Step 2: Hash the bytes using keccak256 and return the result
+  return keccak256(messageBytes);
+}
+
+module.exports = hashMessage;
+
+// turn this into an array of bytes, the expected format for the hash algorithm
+const bytes = utf8ToBytes("Vote Yes on Proposal 327");
+// hash the message using keccak256
+const hash = keccak256(bytes);
+
+console.log(toHex(hash)); // 928c3f25193b338b89d5646bebbfa2436c5daa1d189f9c565079dcae379a43be
