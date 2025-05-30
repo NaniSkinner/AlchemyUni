a918dc5ca870c7edbf66072bf44d1dcd76b1c414
NaniSkinner <skinner.galvez@gmail.com>
2025-05-30 12:13:19 -0500

    mempool adding transcations

--- index.js
+++ index.js
@@ -94,3 +94,31 @@ function getAddress(publicKey) {
 }
 
 module.exports = getAddress;
+
+//MEMPOOL - GOAL: ADD TRANSACTIONS
+
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  // Add the transaction to the mempool array
+  mempool.push(transaction);
+}
+
+function mine() {
+  // TODO: mine a block
+}
+
+module.exports = {
+  TARGET_DIFFICULTY,
+  MAX_TRANSACTIONS,
+  addTransaction,
+  mine,
+  blocks,
+  mempool,
+};
